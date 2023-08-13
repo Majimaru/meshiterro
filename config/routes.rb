57c09cf6b0038b415ac3b31ae69545bd697d89ed
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-08-14 04:53:56 +0900

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    # 単数形にすると、/:idがURLに含まれなくなる
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
